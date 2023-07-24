f18e22e9e43c719c9a83f23748599fc9ea661e30
bensigo <sigocode@gmail.com>
2023-07-24 12:20:52 +0400

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { reportWebVitals } from 'next-axiom'
 
 const inter = Inter({ subsets: ['latin'] })
 
